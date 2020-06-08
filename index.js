e73039faa895cc8c80540026543e3dd3659447e8
arnavdesk <arnavdesk@gmail.com>
2020-06-08 05:41:54 +0530

    save progress of the day

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
 app.use(express.static("./assets"));
+
 // extract style and scripts into sub pages
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
